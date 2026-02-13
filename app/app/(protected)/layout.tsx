579f0aacca5df5cc60e6a8bb818b27a2486fa6c4
Evyatar Druyan <evyatardruyan@Evyatars-MacBook-Air.local>
2026-02-13 16:50:49 +0200

    Fix nullable email in protected layout

--- app/app/(protected)/layout.tsx
+++ app/app/(protected)/layout.tsx
@@ -30,7 +30,7 @@ export default async function ProtectedLayout({ children }: { children: React.Re
     redirect("/login");
   }
 
-  const user = await prisma.user.findUnique({ where: { email } });
+  const user = await prisma.user.findUnique({ where: { email: email ?? undefined } });
   if (!user) {
     redirect("/login");
   }
